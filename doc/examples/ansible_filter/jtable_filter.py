9871bd6b6a926a94225ea502c5e51405077f7aff
vince <vince@vince.com>
2024-10-19 14:59:43 +0200

    fix_ansible_filter

--- doc/examples/ansible_filter/jtable_filter.py
+++ doc/examples/ansible_filter/jtable_filter.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 import sys,os
 
-# print(sys.path)
-# parent_path = (os.path.realpath("f{__file__}/.."))
-# print(parent_path)
-# parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 parent_path = '/'.join(__file__.split("/")[:-4]) + "/jtable"
-# parent_path = '/'.join(__file__.split("/")[:-2])
-# print(parent_path)
-# exit(0)
 sys.path.append(parent_path)
 
 try:
@@ -19,9 +12,6 @@ except:
 # print(dir(jtable))
 
 def jtable_filter(dataset,select=[],path="{}",format="simple",views={}, when=[],queryset={}):
-    # print(jtable.__file__)
-    # print(dataset)
-    # return jtable.JtableCls(render="jinja_ansible").render_object({"stdin": dataset},path=path, select=select)[format]
     return jtable.JtableCls(render="jinja_ansible").render_object( dataset,path=path, select=select,views=views, when=when,format=format, queryset=queryset)
 
 class FilterModule(object):
