85f403b27c646fb8a8fc37faf6199b4a7240c1fc
vince <vince@vince.com>
2024-10-20 17:01:46 +0200

    add_b64_filters

--- jtable/jtable.py
+++ jtable/jtable.py
@@ -95,6 +95,12 @@ logging_config = {
 }
 
 class Filters:
+    def b64decode(value):
+        import base64
+        return base64.b64decode(value).decode('utf-8')
+    def b64encode(value):
+        import base64
+        return base64.b64encode(value.encode('utf-8')).decode('utf-8')
     def jtable(dataset,select=[], unselect=[],path="{}",format="",views={}, when=[],context = {}, queryset={}):
         # logging.info(f"context: {str(context)}",extra={'class_name': 'Filters', 'parent_function': 'jtable'})
         # logging.info("coucou")
@@ -112,7 +118,19 @@ class Filters:
         # logging.info(b)
         # return set(a).intersection(b)
         return list(set(a).intersection(b))
-    
+    def js_wrap(data):
+        header = "<head><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js\"></script><style>table{font-family:arial,sans-serif;border-collapse:collapse;text-align:left;margin-left:2.5%}th,td{border:1px solid #dddddd;text-align:left;padding:8px}tbody tr:nth-child(even){background-color:#f2f2f2}.top-search-container{display:block;align-items:center;margin:5px auto;border-radius:5px}.search-container{align-items:center;border:1px solid #4f1616;border-radius:5px}</style></head>"
+        script = '<script>var tables=document.getElementsByTagName(\'table\');top_search_box="<input type=\'text\' class=\'top-search-container\' onkeyup=\'filterAllTables()\' placeholder=\'filter on all tables\'>\n";$(top_search_box).prependTo("body");console.log(top_search_box);let table_id=0;for(const table of tables){let prepa_search_boxes="<tr>\n";let column_id=0;table.id=table_id;for(const cell of table.tHead.rows.item(0).cells){cell.onclick=(function(table_id,column_id){return function(){sortTable(table_id,column_id);};})(table_id,column_id);prepa_search_boxes=prepa_search_boxes+"<th><input type=\'text\' class=\'search-container\' onkeyup=\'filterTable("+table_id+","+column_id+")\' placeholder=\'filter by "+cell.innerText+"\'></th>\n";column_id++;}prepa_search_boxes=prepa_search_boxes+"</tr>";$(table.tHead).prepend(prepa_search_boxes);table_id++;}function filterTable(table_id,column){var table=document.getElementById(table_id);var input=table.rows[0].cells[column].querySelector("input");var filterText=input.value.toUpperCase();var rows=Array.prototype.slice.call(table.tBodies[0].rows);rows.forEach(function(row){var cellText=row.cells[column].textContent.toUpperCase();if(cellText.indexOf(filterText)>-1){row.style.display="";}else{row.style.display="none";}});}function filterAllTables(){var search_input=$("input.top-search-container").val().toUpperCase();for(const table of tables){var rows=Array.prototype.slice.call(table.tBodies[0].rows);rows.forEach(function(row){var cellText=row.textContent.toUpperCase();if(cellText.indexOf(search_input)>-1){row.style.display="";}else{row.style.display="none";}});}}function sortTable(table_id,column){var table=document.getElementById(table_id);var rows=Array.prototype.slice.call(table.tBodies[0].rows);var isAscending=table.tHead.rows[0].cells[column].classList.toggle("ascending");var multiplier=isAscending?1:-1;rows.sort(function(a,b){var aVal=a.cells[column].textContent;var bVal=b.cells[column].textContent;if(!isNaN(aVal)&&!isNaN(bVal)){aVal=Number(aVal);bVal=Number(bVal);return multiplier*(aVal-bVal);}else{return multiplier*aVal.localeCompare(bVal);}});table.tBodies[0].innerHTML="";rows.forEach(function(row){table.tBodies[0].appendChild(row);});}</script>'
+        return f"""
+          <!DOCTYPE html>
+            <html>
+              {header}
+                <body>
+                    {data}
+                </body>
+              {script}
+            </html>
+          """
     def regex_replace(value="", pattern="", replacement="", ignorecase=False):
         """ Perform a `re.sub` returning a string """
         if ignorecase:
@@ -283,8 +301,8 @@ class JtableCli:
             logging.info("stdin is a pipe")
             for line in sys.stdin:
                 stdin = stdin + line
-            self.dataset = { self.tabulate_var_name: yaml.safe_load(stdin) }
-            # self.dataset = yaml.safe_load(stdin)
+            # self.dataset = { self.tabulate_var_name: yaml.safe_load(stdin) }
+            self.dataset = { self.tabulate_var_name: stdin }
 
         if not is_pipe and not args.json_file and not args.json_files and not args.query_file and not args.yaml_files:
 
