d0dfb2eb0ad2c74162657e323c4cda2a8da74bc7
vince <vince@vince.com>
2024-11-30 19:44:30 +0100

    move_logging_cls_in_module_file

--- jtable/jtable.py
+++ jtable/jtable.py
@@ -3,6 +3,8 @@ import sys, json, re, os, ast, inspect, datetime, time, logging, logging.config,
 from os import isatty
 from sys import exit
 from typing import Any, Dict, Optional
+from logger import CustomFormatter, CustomFilter, _ExcludeErrorsFilter, logging_config
+# import filters as Filters
 
 jtable_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -26,76 +28,6 @@ if running_platform == "Windows":
 else:
     running_os = running_platform
 
-
-class CustomFormatter(logging.Formatter):
-    def format(self, record):
-        frame = inspect.currentframe()
-        
-        parent_function = inspect.getouterframes(frame)[9].function
-        
-        record.parent_function = parent_function
-              
-        try:
-            class_name = inspect.currentframe().f_back.f_back.f_back.f_back.f_back.f_back.f_back.f_back.f_back.f_locals["self"].__class__.__name__
-        except:
-            class_name = "unknown"
-        class_name = class_name.lower().replace("jtable","")
-        record.class_name = class_name
-        return super().format(record)
-
-class CustomFilter(logging.Filter):
-    def filter(self, record):
-        record.class_name = getattr(record, 'class_name', 'UnknownClass')
-        record.parent_function = getattr(record, 'parent_function', 'UnknownFunction')
-        
-        context = f"{record.class_name}.{record.parent_function}"
-        record.fixed_context = f"{context:<50}"
-        
-        total_message_size = len(record.fixed_context) + len(record.msg)
-        # logging.info(f"total_message_size: {str(total_message_size)}")
-        # print(f"total_message_size: {str(total_message_size)}")
-        if total_message_size > terminal_size.columns:
-            record.msg = str(record.msg)[:terminal_size.columns - len(record.fixed_context) ] + '...'
-
-        return True
-
-class _ExcludeErrorsFilter(logging.Filter):
-    def filter(self, record):
-        """Only lets through log messages with log level below ERROR ."""
-        return record.levelno < logging.ERROR
-
-"""
-https://stackoverflow.com/questions/14058453/making-python-loggers-output-all-messages-to-stdout-in-addition-to-log-file
-"""
-logging_config = {
-    'version': 1,
-    'formatters': {
-        'my_formatter': {
-            '()': CustomFormatter,
-            # 'format': '%(asctime)s (%(lineno)s) %(class_name)s.%(parent_function)s | %(levelname)s %(message)s',
-            'datefmt': '%H:%M:%S'
-
-        }
-    },
-    'filters': {
-        'custom': {
-            '()': CustomFilter,
-        },
-    },
-    'handlers': {
-        'console_stderr': {
-            'class': 'logging.StreamHandler',
-            'level': 'ERROR',
-            'formatter': 'my_formatter',
-            'stream': sys.stderr,
-            'filters': ['custom'],
-        },
-    },
-    'root': {
-        'level': 'NOTSET',
-        'handlers': ['console_stderr']
-    },
-}
 
 class Filters:
     def b64decode(value):
@@ -1086,10 +1018,11 @@ class Templater:
         if 'Environment' not in sys.modules:
             from jinja2 import Environment, StrictUndefined
         env = Environment(undefined=StrictUndefined)
-        
+
         jtable_core_filters = [name[0] for name in inspect.getmembers(Filters, predicate=inspect.isfunction)]
         for filter_name in jtable_core_filters:
             env.filters[filter_name] = getattr(Filters, filter_name)
+        logging.info(f"jtable_core_filters: {jtable_core_filters}")
 
         ####################  Add plugin function ####################
         jtable_core_plugins = [name[0] for name in inspect.getmembers(Plugin, predicate=inspect.isfunction)]
--- jtable/logger.py
+++ jtable/logger.py
@@ -1,44 +1,71 @@
-#!/usr/bin/env python3
-import logging
-import inspect
+import logging,inspect,sys
 
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        frame = inspect.currentframe()
+        
+        parent_function = inspect.getouterframes(frame)[9].function
+        
+        record.parent_function = parent_function
+              
+        try:
+            class_name = inspect.currentframe().f_back.f_back.f_back.f_back.f_back.f_back.f_back.f_back.f_back.f_locals["self"].__class__.__name__
+        except:
+            class_name = "unknown"
+        class_name = class_name.lower().replace("jtable","")
+        record.class_name = class_name
+        return super().format(record)
 
-def setup_logger_alias():
-    frame = inspect.currentframe()
-    try:
-        while frame:
-            local_vars = frame.f_locals
-            if "self" in local_vars:
-                instance = local_vars["self"]
-                if hasattr(instance, "logger"):
-                    return instance.logger
-            frame = frame.f_back
-    finally:
-        del frame  # Nettoyage explicite pour éviter des cycles de référence
-    raise RuntimeError("Impossible de trouver `self.logger` dans la pile.")
+class CustomFilter(logging.Filter):
+    def filter(self, record):
+        record.class_name = getattr(record, 'class_name', 'UnknownClass')
+        record.parent_function = getattr(record, 'parent_function', 'UnknownFunction')
+        
+        context = f"{record.class_name}.{record.parent_function}"
+        record.fixed_context = f"{context:<50}"
+        
+        # total_message_size = len(record.fixed_context) + len(record.msg)
+        # logging.info(f"total_message_size: {str(total_message_size)}")
+        # print(f"total_message_size: {str(total_message_size)}")
+        # if total_message_size > terminal_size.columns:
+        #     record.msg = str(record.msg)[:terminal_size.columns - len(record.fixed_context) ] + '...'
 
-def with_logger(cls):
-    """
-    Décorateur pour ajouter un logger à une classe tout en conservant le nom d'origine.
-    """
-    class WrappedClass(cls):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+        return True
 
-            self.logger = logging.getLogger(cls.__name__)
-            self.logger.setLevel(logging.DEBUG)
+class _ExcludeErrorsFilter(logging.Filter):
+    def filter(self, record):
+        """Only lets through log messages with log level below ERROR ."""
+        return record.levelno < logging.ERROR
 
-            if not self.logger.handlers:
-                handler = logging.StreamHandler()
-                formatter = logging.Formatter(
-                    '%(asctime)s.%(msecs)03d  - %(name)-20s - %(levelname)s - %(message)s',
-                    '%H:%M:%S'
-                )
+"""
+https://stackoverflow.com/questions/14058453/making-python-loggers-output-all-messages-to-stdout-in-addition-to-log-file
+"""
+logging_config = {
+    'version': 1,
+    'formatters': {
+        'my_formatter': {
+            '()': CustomFormatter,
+            # 'format': '%(asctime)s (%(lineno)s) %(class_name)s.%(parent_function)s | %(levelname)s %(message)s',
+            'datefmt': '%H:%M:%S'
 
-                handler.setFormatter(formatter)
-                self.logger.addHandler(handler)
-
-
-    WrappedClass.__name__ = cls.__name__
-    WrappedClass.__qualname__ = cls.__qualname__
-    return WrappedClass
+        }
+    },
+    'filters': {
+        'custom': {
+            '()': CustomFilter,
+        },
+    },
+    'handlers': {
+        'console_stderr': {
+            'class': 'logging.StreamHandler',
+            'level': 'ERROR',
+            'formatter': 'my_formatter',
+            'stream': sys.stderr,
+            'filters': ['custom'],
+        },
+    },
+    'root': {
+        'level': 'NOTSET',
+        'handlers': ['console_stderr']
+    },
+}
